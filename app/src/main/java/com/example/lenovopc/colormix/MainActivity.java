7fac6f3d3be6b420e8c61a926562feb08b556dd6
Mohammed Deifallah <mohammed_deifallah@yahoo.com>
2018-05-13 17:59:09 +0200

    If wrong, the background gets the right color

--- app/src/main/java/com/example/lenovopc/colormix/MainActivity.java
+++ app/src/main/java/com/example/lenovopc/colormix/MainActivity.java
@@ -1,29 +1,33 @@
 package com.example.lenovopc.colormix;
 
+import android.annotation.SuppressLint;
 import android.graphics.drawable.GradientDrawable;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
     private ColorGenerator cg;
-    private ImageView circle1, circle2, ops[];
+    private LinearLayout back;
     private TextView score, center;
+    private GradientDrawable circles[], g[];
     private Random random;
-    private int correct_index;
+    private ImageView[] ops;
+    private int correct_index, correct_color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        circle1 = findViewById(R.id.circle1);
-        circle2 = findViewById(R.id.circle2);
-        //GradientDrawable g1, g2, g3, g4, g5;
+        ImageView circle1 = findViewById(R.id.circle1);
+        ImageView circle2 = findViewById(R.id.circle2);
+        back = findViewById(R.id.back);
         ops = new ImageView[3];
         ops[0] = findViewById(R.id.op1);
         ops[0].setOnClickListener(new View.OnClickListener() {
@@ -47,42 +51,68 @@ public class MainActivity extends AppCompatActivity {
         center = findViewById(R.id.center);
         cg = new ColorGenerator();
         random = new Random();
+        circles = new GradientDrawable[2];
+        for (int i = 0; i < 2; i++) {
+            circles[i] = new GradientDrawable();
+            circles[i].setGradientType(GradientDrawable.LINEAR_GRADIENT);
+            circles[i].setShape(GradientDrawable.OVAL);
+            circles[i].setSize(50, 50);
+        }
+        circle1.setImageDrawable(circles[0]);
+        circle2.setImageDrawable(circles[1]);
+        g = new GradientDrawable[3];
+        for (int i = 0; i < 3; i++) {
+            g[i] = new GradientDrawable();
+            g[i].setGradientType(GradientDrawable.LINEAR_GRADIENT);
+            g[i].setShape(GradientDrawable.OVAL);
+            g[i].setSize(50, 50);
+            ops[i].setImageDrawable(g[i]);
+        }
         newGame();
     }
 
-    void createGradients(int color, ImageView view) {
-        GradientDrawable g = new GradientDrawable();
+    void newGame() {
+        int c1 = cg.getRandomColor();
+        int c2 = cg.getRandomColor();
+        correct_color = cg.getColorMix(c1, c2);
+        int c4 = cg.getRandomColor();
+        int c5 = cg.getRandomColor();
+        createGradients(c1, circles[0]);
+        createGradients(c2, circles[1]);
+        createOptions(correct_color, c4, c5);
+    }
+
+    void createGradients(int color,/* ImageView view,*/ GradientDrawable g) {
         g.setColor(color);
-        g.setGradientType(GradientDrawable.LINEAR_GRADIENT);
-        g.setShape(GradientDrawable.OVAL);
-        g.setSize(50, 50);
-        view.setImageDrawable(g);
     }
 
-    int getRandomIndex() {
-        return Math.abs(random.nextInt() % 3);
-    }
-
-    void createOptions(ImageView[] ops, int c1, int c2, int c3) {
+    void createOptions(int c1, int c2, int c3) {
         int correct = getRandomIndex();
         correct_index = correct;
         int wrong1 = (correct ^ 3) % 2;
         int wrong2 = (correct ^ 3) & 2;
-        createGradients(c1, ops[correct]);
-        createGradients(c2, ops[wrong1]);
-        createGradients(c3, ops[wrong2]);
+        for (int i = 0; i < 3; i++) {
+            ops[i].setVisibility(View.VISIBLE);
+        }
+        createGradients(c1, g[correct]);
+        createGradients(c2, g[wrong1]);
+        createGradients(c3, g[wrong2]);
+    }
+
+
+    int getRandomIndex() {
+        return Math.abs(random.nextInt() % 3);
     }
 
     void check(int index) {
         if (index == correct_index) {
             correct();
         } else {
-
-                wrong();
-
+            wrong();
         }
     }
 
+    @SuppressLint("SetTextI18n")
     void correct() {
         Integer s = Integer.parseInt(score.getText().toString());
         s++;
@@ -91,22 +121,17 @@ public class MainActivity extends AppCompatActivity {
         newGame();
     }
 
-    void wrong(){
+    @SuppressLint("SetTextI18n")
+    void wrong() {
         center.setText("Try Again :(");
+        showCorrect();
         Integer s = Integer.parseInt(score.getText().toString());
         s--;
         score.setText(s.toString());
         newGame();
     }
 
-    void newGame() {
-        int c1 = cg.getRandomColor();
-        int c2 = cg.getRandomColor();
-        int c3 = cg.getColorMix(c1, c2);
-        int c4 = cg.getRandomColor();
-        int c5 = cg.getRandomColor();
-        createGradients(c1, circle1);
-        createGradients(c2, circle2);
-        createOptions(ops, c3, c4, c5);
+    void showCorrect() {
+        back.setBackgroundColor(correct_color);
     }
 }
